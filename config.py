a37162a4cdf3527835c0a6dbf55f90fc04d0eaad
david joseph <codedjade003@gmail.com>
2024-09-30 20:21:04 +0000

    Api

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Configuration for the Orderly Network and Empyreal SDK
 ORDERLY_API_KEY = 'your_orderly_api_key'
 ORDERLY_SECRET_KEY = 'your_orderly_secret_key'
-EMPYREAL_API_KEY = 'your_empyreal_api_key'
+EMPYREAL_API_KEY = '0MEeeRanXpcOcBOUemkvCxoAj-PPlg'
